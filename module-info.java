f8bbcdf9e61a65aa25de46e1db702b2b8e78e92e
Rage Man <53162297+rage23403@users.noreply.github.com>
2022-05-18 08:05:32 -0500

    comments added

--- module-info.java
+++ module-info.java
@@ -3,8 +3,8 @@ module TwoDTest {
 	requires com.fasterxml.jackson.databind;
 	requires com.fasterxml.jackson.annotation;
 	requires transitive javafx.controls;
-	exports application;
-	exports misc;
+	exports application; //required to use JSONComp's "load" function on classes in application.
+	exports misc; //recommended to use any of misc's files/functions inside the application project. removal only causes warnings
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
